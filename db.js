a611d1807648017e08666e150f927fad64986bb5
Rodrigo <rodrigovf2102@gmail.com>
2022-09-09 17:39:36 -0300

    backend project

---
+++ db.js
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+try {
+    await mongoClient.connect()
+} catch (error) {
+    console.log(error.message)
+} 
+const db = mongoClient.db('mywallet-backend');
+
+export default db;
